46fd15858537be4edc2e5bfae0aa87ce16384171
BK -40 <bkhadiza277@gmail.com>
2021-08-03 12:56:33 +0600

    while loop with even and odd number

---
+++ whileLoop.js
@@ -0,0 +1,19 @@
+// dfghgdfjg  control slash
+var number =0;
+
+while (number <=10){
+    console.log(number);
+    number++;
+}
+var odd =1;
+var even =2;
+console.log("Odd number from 1 to 20");
+while (odd <= 20){
+    console.log(odd);
+    odd += 2;
+}
+console.log("Even number from 1 to 20");
+while (even <= 20){
+    console.log(even);
+    even += 2;
+}

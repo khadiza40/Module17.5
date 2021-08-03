bd30f98c6458db79d086dbaaa029128e490903ae
BK -40 <bkhadiza277@gmail.com>
2021-08-03 13:31:20 +0600

    for loop

---
+++ forLoop.js
@@ -0,0 +1,10 @@
+for (var num =1;  num<10 ; num++){
+    console.log(num);
+}
+
+console.log("All array elements! ");
+var numbers = [22,35, 23,14,543,21,12];
+for ( var i = 0; i<numbers.length; i++){
+    // var elements = numbers[i];
+    console.log(numbers[i]);
+}
